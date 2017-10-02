e136b3304e8b6dd3b1f770b1d1d4927e86a5da70
bigbugbb <bigbugbb@gmail.com>
2017-10-02 15:43:13 +0800

    Update navigation bar, add more assets, add shared resourcesr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
-import { AppComponent } from './app.component';
+import { NAV_DROPDOWN_DIRECTIVES } from './shared/nav-dropdown.directive';
+import { SIDEBAR_TOGGLE_DIRECTIVES } from './shared/sidebar.directive';
+import { AsideToggleDirective } from './shared/aside.directive';
+import { BreadcrumbsComponent } from './shared/breadcrumb.component';
 
 // Routing
 import { AppRoutingModule } from './app.routing';
@@ -18,19 +23,27 @@ import { AuthGuard } from './_guards/auth.guard';
 import { AppLayoutComponent } from './_layouts/app-layout.component';
 import { AuthComponent, LogoutComponent } from './auth';
 
+import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     AppLayoutComponent,
     AuthComponent,
-    LogoutComponent
+    LogoutComponent,
+    NAV_DROPDOWN_DIRECTIVES,
+    BreadcrumbsComponent,
+    SIDEBAR_TOGGLE_DIRECTIVES,
+    AsideToggleDirective,
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TabsModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
   providers: [
     AuthGuard, 
