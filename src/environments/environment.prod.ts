a18814288d9e8aa10dada9afe4f8a477182e59e2
bigbug <bigbugbb@gmail.com>
2017-10-21 10:59:47 +0800

    Update environment config for s3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: true,
   apiEndpoint: 'https://fast-ocean-42635.herokuapp.com/api/v1',
   noeFilesUpload: 'noe-file-upload',
-  bucketRegion: 'ap-northeast-2',
-  identityPoolId: 'ap-northeast-2:7d43ae4b-c5fb-4bea-8057-7f771406a078'
+  // bucketRegion: 'ap-northeast-2',
+  // identityPoolId: 'ap-northeast-2:7d43ae4b-c5fb-4bea-8057-7f771406a078',
+  bucketRegion: 'ap-northeast-1',
+  identityPoolId: 'ap-northeast-1:3206cef3-eb3a-4062-a0a2-9b519fa5f9c9'
 };
